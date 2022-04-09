252b4a680ba6cfd6965013fe1114db1757caddc6
Paul Holsters <paul.holsters@hotmail.com>
2022-04-10 00:15:59 +0200

    - zien dat specs die niet langer in de database staan uit de UI verwijderd worden als je ze naar de linkerkolom verplaatst worden: eerst even testen of dat andere werkt : ok dan dialog die het zaakje opruimde als je akkoord bent: deel 1

--- client/src/app/quotations/quotation-flow/quotation-specifications/quotation-specification-picklist/quotation-specification-picklist.component.ts
+++ client/src/app/quotations/quotation-flow/quotation-specifications/quotation-specification-picklist/quotation-specification-picklist.component.ts
@@ -18,6 +18,7 @@ export class QuotationSpecificationPicklistComponent implements OnInit {
   editedQuotationSpecification:QuotationSpecificationModel|undefined
   loadedQuotationSpecification:QuotationSpecificationModel|undefined
   displayEditQuotationSpecificationDialog:boolean
+  header:string|undefined
   constructor(private messageService: MessageService, storage: QuotationStorageService, private dataService: DataService,
               private confirmationService:ConfirmationService) {
     this.displayEditQuotationSpecificationDialog = false
@@ -29,9 +30,11 @@ export class QuotationSpecificationPicklistComponent implements OnInit {
   }
 
   deleteQuotationSpecification(id:string){
+    this.header = 'Verwijderen offerte specificatie'
     this.confirmationService.confirm({
       message: 'Het verwijderen is definitief. Doorgaan?',
       accept: ()=>{
+        this.header = undefined
         this.dataService.deleteQuotationSpecification(id).subscribe(res=>{
           // inform parent to rerender this component
           this.source.splice(this.source.findIndex(opt=>{
@@ -40,14 +43,60 @@ export class QuotationSpecificationPicklistComponent implements OnInit {
           this.listChanged.emit({source:this.source,target:this.target})
           this.messageService.add({severity:'success', summary: 'Offerte specificatie verwijderd', life:3000});
         })
+      },
+      reject: ()=>{
+        this.header = undefined
       }
     })
   }
 
-  store(){
-    // call the store function of the parent without rerendering this component
-    // while passing the source and the target
-    // the the parent should update its lists via the store ?
+  onMoveToSource(quotationSpecsMoved:{items:QuotationSpecificationModel[]}){
+    // neem wat er overgezet wordt
+    // indien specificatie die niet lange r bestaat in db toon dialoogbox
+    // in deze box vermeld dat deze specificatie niet langer bestaat in de db
+    // en dus voor deze offerte na op ok te drukken niet meer kan worden toegevoegd
+    // zorg dat bij annuleren de actie naar source niet meer wordt uitgveoerd
+    this.dataService.getQuotationSpecifications().subscribe((quotspecs:QuotationSpecificationModel[])=>{
+      const ids = quotspecs.map(sp=>{
+        return sp._id
+      })
+      const deletedIds:string[] = []
+      for (let specMoved of quotationSpecsMoved.items){
+        if(!specMoved._id || !ids.includes(specMoved._id)){
+          deletedIds.push(specMoved._id||'')
+        }
+      }
+      if(deletedIds.length>0){
+        this.header = 'Speciaal verwijderen'
+        let msg = 'De specificatie die u uit de lijst verwijdert, bestaat niet langer in de database.' +
+            ' Indien u op ja klikt wordt de specificatie ook definitief verwijderd uit huidige offerte.'
+        if(deletedIds.length>1) msg = 'Er bevinden zich '+ deletedIds.length +' specificaties die niet langer in de database bestaan. Indien u op ja klikt' +
+            ' worden deze ook definitief verwijderd uit huidige offerte.'
+        this.confirmationService.confirm({
+          message: msg,
+          accept: ()=>{
+            this.header = undefined
+            deletedIds.forEach(id=>{
+              const index = this.source.findIndex(sp=>{
+                return sp._id?.toString()===id.toString()
+              })
+              this.source.splice(index,1)
+            })
+            // todo should be a reload event instead
+            this.storeEvent.emit({source:this.source,target:this.target})
+          },
+          reject: ()=>{
+            this.header = undefined
+            this.storeEvent.emit({source:this.source,target:this.target})
+          }
+        })
+      } else{
+        this.storeEvent.emit({source:this.source,target:this.target})
+      }
+    })
+  }
+
+  onMoveToTarget(event:any){
     this.storeEvent.emit({source:this.source,target:this.target})
   }
 
